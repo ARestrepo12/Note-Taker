5021eb68a1826ac73c4da56f32e761caa316ace2
unknown <arestre@ncsu.edu>
2022-06-30 21:11:04 -0400

    setup express server and test GET route

---
+++ server.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const notes = require('./Develop/data/notes.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
