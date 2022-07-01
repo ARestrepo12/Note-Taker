79f1f0e8fe0d2f04bbd7b50136c84e64bb0d0787
unknown <arestre@ncsu.edu>
2022-06-30 21:50:54 -0400

    add function to create new notes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -7,14 +9,26 @@ app.use(express.json());
 
 const allNotes = require('./Develop/data/notes.json');
 
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/data/notes.json'),
+        JSON.stringify({ allNotes: notesArray }, null, 2)
+    );
+    return newNote;
+}
 app.get('/api/notes', (req, res) => {
     res.json(allNotes);
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body)
-    res.json(req.body);
-})
+    req.body.id = allNotes.length.toString();
+
+    const newNote = createNewNote(req.body, allNotes);
+
+    res.json(newNote);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
