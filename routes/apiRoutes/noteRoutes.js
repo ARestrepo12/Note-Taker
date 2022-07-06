c79b20b002898efbb3cd736630e1843c4d8b6878
unknown <arestre@ncsu.edu>
2022-07-06 02:13:45 -0400

    add html route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,2 +1,9 @@
 const router = require('express').Router();
-const { notes } = require('./data/notes.json');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../../public/notes.html));
+});
+
+
+module.exports = router
