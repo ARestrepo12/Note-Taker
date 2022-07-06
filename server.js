ccd356139bc0b8e0d35029aa51e43724ce4d661b
unknown <arestre@ncsu.edu>
2022-07-06 01:44:23 -0400

    grading changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: ture}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static('public')); 
