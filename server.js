ce691a9caff58f28b36aa8daf568e197b392a681
unknown <arestre@ncsu.edu>
2022-07-06 02:01:22 -0400

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -9,37 +10,9 @@ app.use(express.json());
 
 app.use(express.static('public')); 
 
-const allNotes = require('./Develop/data/notes.json');
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './Develop/data/notes.json'),
-        JSON.stringify({ allNotes: notesArray }, null, 2)
-    );
-    return newNote;
-}
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes);
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = allNotes.length.toString();
-
-    const newNote = createNewNote(req.body, allNotes);
-
-    res.json(newNote);
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-})
+});
